2b6331f56f50580c2216e37b5031a98631aece51
Kirill <kirill.lihotin@gmail.com>
2020-01-22 17:24:08 +0500

    8

--- Drivers/CacheRedis.php
+++ Drivers/CacheRedis.php
@@ -3,27 +3,36 @@ declare(strict_types=1);
 
 class CacheRedis implements CacheInterface
 {
-    public function __construct()
+    private $connection;
+
+    public function __construct('127.0.0.1', 6379)
     {
         $this->connection = new Redis();
     }
 
     public function set(string $key, $value): void
     {
-        $this->connection->$key = $value;
-    }
-
-    public function get(string $key)
-    {
-        if (array_key_exists($key, $this->connection)) {
-            return $this->connection->$key;
-        } else {
-            return null;
+        $fileData = file_get_contents($this->connection);
+        $unserialized = @unserialize($fileData);
+        if (!is_array($unserialized)) {
+            $unserialized = [];
         }
+        $unserialized->$key = $value;
+        $serialized = serialize($unserialized);
+        file_put_contents($this->connection, $serialized);
     }
 
+    public function get($key)
+    {   
+        $file_data = file_get_contents($this->connection);
+        $unserialized = unserialize($file_data);
+        if (array_key_exists($key, $unserialized)) {
+            return $unserialized[$key];
+        }
+        return null;
+    }
     public function remove(string $key): void
     {
-        unset($this->connection->$key);
+        
     }
 }
