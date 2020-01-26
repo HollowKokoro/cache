efffb4e07adac0643f605883468d9f374a1e7991
HollowKokoro <kirill.lihotin@gmail.com>
2020-01-26 19:07:30 +0500

    22

--- Drivers/CacheRedisFsocket.php
+++ Drivers/CacheRedisFsocket.php
@@ -25,7 +25,7 @@ class CacheRedis implements CacheInterface
     {
         $serialized = serialize($value);
         $data = $serialized[$key];
-        fwrite($this->connection, 'connection->set(\'$key\', \'$data\'');
+        fwrite($this->connection, 'set(\'$key\', \'$data\'');
     }
 
     /**
@@ -33,8 +33,8 @@ class CacheRedis implements CacheInterface
      */
     public function get(string $key)
     {
-        $unserialized = unserialize($this->connection);
-        fread($unserialized, 100);
+        fwrite($this->connection, 'get(\'$key\')');
+        
     }
 
     /**
