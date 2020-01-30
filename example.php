00387d29b7a90301ece4dea7798bc32eb5e1b331
HollowKokoro <kirill.lihotin@gmail.com>
2020-01-30 22:26:26 +0500

    32

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once '/home/kokoro/Cache/Interfaces/CacheInterface.php';
-require_once '/home/kokoro/Cache/Drivers/CacheRedisFsocket.php';
+require_once '/home/kokoro/Cache/Drivers/CacheRedisFsocketNew.php';
 
-$values = new CacheRedisFsocket("127.0.0.1", 6379);
+$values = new CacheRedisFsocketNew("127.0.0.1", 6379);
 $values->set("test", "Hello");
-//echo $values->get("test");
-//$values->remove('test');
+echo $values->get("test");
+$values->remove('test');
