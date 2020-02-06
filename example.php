fb0fcc0f8c0c57288f58e69cef102833020364c4
Kirill <kirill.lihotin@gmail.com>
2020-02-06 18:57:02 +0500

    50

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
 require_once '/home/kokoro/Cache/Interfaces/CacheInterface.php';
 require_once '/home/kokoro/Cache/Drivers/CacheRedisFsocket.php';
 
-$values = new CacheRedisFsocket("127.0.0.1", 6379, -999999999999);
+$values = new CacheRedisFsocket("127.0.0.1", 6379, 0);
 $values->set("test", "Hello");
 $values->get("test");
 $values->remove("test");
