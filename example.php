0219428e7a622b33b3a6356582f81699781c0183
Kirill <kirill.lihotin@gmail.com>
2020-02-19 17:06:25 +0500

    50

--- example.php
+++ example.php
@@ -8,8 +8,8 @@ require_once "/home/kokoro/Cache/Values/ValueNotFound.php";
 require_once "/home/kokoro/Cache/Values/ValueFound.php";
 
 $values = new CacheRedisFsocket("127.0.0.1", 6379, 0);
+$y = new ValueFound("test");
 $x = $values->get("test");
-if ($x !== $x->isFound(false))
-{
+if ($values->get($y) === $values->get("test")) {
     print_r($x);
 };
