458408cda45d5c73e8bc93f7ab8fd58d5de9b4cf
Kirill <kirill.lihotin@gmail.com>
2020-03-10 17:04:10 +0500

    180

--- example.php
+++ example.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 
 require_once "./Interfaces/CacheInterface.php";
-require_once "./Drivers/CacheFile.php";
+//require_once "./Drivers/CacheFile.php";
 //require_once "./Drivers/CacheRedisFsocket.php";
+require_once "./Drivers/CacheRedis.php";
 require_once "./Interfaces/ValueInterface.php";
 require_once "./Values/ValueNotFound.php";
 require_once "./Values/ValueFound.php";
 
-//$cache = new CacheRedisFsocket("127.0.0.1", 6379, 0);
-$cache = new CacheFile("./1.txt");
+$cache = new CacheRedis("127.0.0.1", 6379, 1);
 $cache->set("test", "Hello", 453);
 print_r($cache->get("test"));
 $cache->remove("test");
